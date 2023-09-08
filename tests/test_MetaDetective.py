e068b14c9969b9bafa4e534fdf59aa42797e7e46
Franck FERMAN <fferman@protonmail.ch>
2023-09-08 02:53:40 +0200

    Temporary and incomplete modification of the test file.

--- tests/test_MetaDetective.py
+++ tests/test_MetaDetective.py
@@ -1,194 +1,260 @@
 import unittest
-from collections import defaultdict, namedtuple
-from io import StringIO
-from unittest.mock import patch, Mock
-import subprocess
-import webbrowser
-import sys
-import os
-
-test_folder = os.path.dirname(os.path.abspath(__file__))
-parent_folder = os.path.dirname(test_folder)
-sys.path.append(parent_folder)
-import MetaDetective as md
-
-Args = namedtuple('Args', ['directory', 'files', 'type'])
+from unittest.mock import patch, MagicMock
+from MetaDetective import *
 
 class TestMetaDetective(unittest.TestCase):
 
-    def setUp(self):
-        self.sample_output = """
-        ExifTool Version Number         : 10.80
-        File Name                       : sample.jpg
-        Directory                       : .
-        File Type                       : JPEG
-        File Type Extension             : jpg
-        MIME Type                       : image/jpeg
+    def test_show_banner(self):
+        with patch('builtins.print') as mocked_print:
+            show_banner()
+            mocked_print.assert_called_with(BANNER)
+
+    def test_open_browser_successful(self):
+        with patch('webbrowser.get') as mocked_get:
+            browser = MagicMock()
+            browser.open.return_value = True
+            mocked_get.return_value = browser
+            result = open_browser('https://example.com')
+            self.assertTrue(result)
+
+    def test_open_browser_failure(self):
+        with patch('webbrowser.get') as mocked_get:
+            browser = MagicMock()
+            browser.open.side_effect = webbrowser.Error
+            mocked_get.return_value = browser
+            result = open_browser('https://example.com')
+            self.assertFalse(result)
+
+    def test_check_exiftool_installed_installed(self):
+        with patch('subprocess.run') as mocked_run:
+            # Create a MagicMock to simulate a successful run
+            mock_run = unittest.mock.MagicMock()
+            mock_run.return_value.returncode = 0
+            mocked_run.return_value = mock_run
+            
+            try:
+                check_exiftool_installed()
+            except SystemExit as se:
+                self.fail(f"check_exiftool_installed() raised SystemExit unexpectedly: {se}")
+
+    def test_check_exiftool_installed_not_installed(self):
+        with patch('subprocess.run') as mocked_run:
+            mocked_run.side_effect = FileNotFoundError
+            with self.assertRaises(SystemExit) as cm:
+                check_exiftool_installed()
+            self.assertEqual(str(cm.exception), 'Error: exiftool is not installed. Please install it to continue.')
+
+    def test_check_exiftool_installed_execution_error(self):
+        with patch('subprocess.run') as mocked_run:
+            mocked_run.side_effect = subprocess.CalledProcessError(1, cmd="exiftool")
+            with self.assertRaises(SystemExit) as cm:
+                check_exiftool_installed()
+            self.assertEqual(str(cm.exception), 'Error: exiftool encountered an error.')
+
+    def test_dms_to_dd_North_East(self):
+        degrees = 45
+        minutes = 30
+        seconds = 15.5
+        direction = 'N'
+        result = dms_to_dd(degrees, minutes, seconds, direction)
+        expected_result = 45.504305555555556
+        self.assertAlmostEqual(result, expected_result, places=6)
+
+    def test_dms_to_dd_South_West(self):
+        degrees = 45
+        minutes = 30
+        seconds = 15.5
+        direction = 'S'
+        result = dms_to_dd(degrees, minutes, seconds, direction)
+        expected_result = -45.504305555555556
+        self.assertAlmostEqual(result, expected_result, places=6)
+
+    def test_dms_to_dd_Zero_Degrees(self):
+        degrees = 0
+        minutes = 0
+        seconds = 0.0
+        direction = 'N'
+        result = dms_to_dd(degrees, minutes, seconds, direction)
+        expected_result = 0.0
+        self.assertEqual(result, expected_result)
+
+    def test_parse_dms_valid(self):
+        dms_str = "50 deg 49' 8.59\" N"
+        result = parse_dms(dms_str)
+        expected_result = (50, 49, 8.59, 'N')
+        self.assertEqual(result, expected_result)
+
+    def test_parse_dms_invalid(self):
+        # Test parsing an invalid DMS string
+        dms_str = "Invalid DMS string"
+        result = parse_dms(dms_str)
+        self.assertIsNone(result)
+
+    def test_parse_dms_no_degrees(self):
+        dms_str = "49' 8.59\" N"
+        result = parse_dms(dms_str)
+        self.assertIsNone(result)
+
+    def test_parse_dms_no_minutes(self):
+        dms_str = "50 deg 8.59\" N"
+        result = parse_dms(dms_str)
+        self.assertIsNone(result)
+
+    def test_parse_dms_no_seconds(self):
+        dms_str = "50 deg 49' N"
+        result = parse_dms(dms_str)
+        self.assertIsNone(result)
+
+    def test_parse_dms_no_direction(self):
+        dms_str = "50 deg 49' 8.59\""
+        result = parse_dms(dms_str)
+        self.assertIsNone(result)
+
+    def test_get_metadata_valid(self):
+        file_path = "valid_file.jpg"
+        fields = ["Title", "Description", "GPS Position"]
+        
+        exiftool_output = """
+            Title: Sample Title
+            Description: This is a sample description
+            GPS Position: 40 deg 30' 12.34\" N, 73 deg 59' 30.56\" W
         """
-        self.expected_metadata = {
-            "File Name": "sample.jpg",
-            "File Type": "JPEG",
-            "File Type Extension": "jpg"
+        with patch('subprocess.run') as mocked_run:
+            mocked_run.return_value.stdout = exiftool_output
+            result = get_metadata(file_path, fields)
+        
+        expected_result = {
+            "Title": "Sample Title",
+            "Description": "This is a sample description",
+            "GPS Position": "40 deg 30' 12.34\" N, 73 deg 59' 30.56\" W",
+            "Formatted GPS Position": "40.503428, -73.991822"
         }
+        self.assertEqual(result, expected_result)
 
-    @patch('subprocess.run')
-    def test_exiftool_installed(self, mock_run):
-        mock_run.return_value = Mock()
-        try:
-            md.check_exiftool_installed()
-        except SystemExit as e:
-            self.fail("check_exiftool_installed() raised SystemExit unexpectedly!")
+    def test_get_metadata_missing_fields(self):
+        file_path = "missing_fields.jpg"
+        fields = ["Title", "Description", "GPS Position"]
+        
+        exiftool_output = """
+            Title: Sample Title
+        """
+        with patch('subprocess.run') as mocked_run:
+            mocked_run.return_value.stdout = exiftool_output
+            result = get_metadata(file_path, fields)
+        
+        expected_result = {
+            "Title": "Sample Title"
+        }
+        self.assertEqual(result, expected_result)
 
-    @patch('subprocess.run')
-    def test_exiftool_not_installed(self, mock_run):
-        mock_run.side_effect = FileNotFoundError()
-        with self.assertRaises(SystemExit) as cm:
-            md.check_exiftool_installed()
-        self.assertEqual(cm.exception.code, md.EXIFTOOL_NOT_INSTALLED)
+    def test_get_metadata_no_gps(self):
+        file_path = "no_gps.jpg"
+        fields = ["Title", "Description", "GPS Position"]
+        
+        exiftool_output = """
+            Title: Sample Title
+            Description: This is a sample description
+        """
+        with patch('subprocess.run') as mocked_run:
+            mocked_run.return_value.stdout = exiftool_output
+            result = get_metadata(file_path, fields)
+        
+        expected_result = {
+            "Title": "Sample Title",
+            "Description": "This is a sample description"
+        }
+        self.assertEqual(result, expected_result)
 
-    @patch('subprocess.run')
-    def test_exiftool_execution_error(self, mock_run):
-        mock_run.side_effect = subprocess.CalledProcessError(1, 'cmd')
-        with self.assertRaises(SystemExit) as cm:
-            md.check_exiftool_installed()
-        self.assertEqual(cm.exception.code, md.EXIFTOOL_EXECUTION_ERROR)
-
-    @patch('subprocess.run')
-    def test_get_metadata(self, mock_run):
-        mock_result = Mock()
-        mock_result.stdout = self.sample_output
-        mock_run.return_value = mock_result
-
-        result = md.get_metadata("sample.jpg", list(self.expected_metadata.keys()))
-
-        self.assertDictEqual(result, self.expected_metadata)
-
-    def test_matches_any_pattern_match_single(self):
-        """Test when value matches a single pattern."""
-        value = "Hello world"
-        patterns = ["^Hello", "not present", "absent"]
-        self.assertTrue(md.matches_any_pattern(value, patterns))
+    def test_matches_any_pattern_match(self):
+        value = "Sample Text"
+        patterns = ["sample", "test", "pattern"]
+        result = matches_any_pattern(value, patterns)
+        self.assertTrue(result)
 
     def test_matches_any_pattern_no_match(self):
-        """Test when value doesn't match any pattern."""
-        value = "Hello world"
-        patterns = ["not present", "absent", "missing"]
-        self.assertFalse(md.matches_any_pattern(value, patterns))
-
-    def test_matches_any_pattern_match_multiple(self):
-        """Test when value matches multiple patterns."""
-        value = "Hello world"
-        patterns = ["^Hello", "world$", "llo"]
-        self.assertTrue(md.matches_any_pattern(value, patterns))
+        value = "Another Text"
+        patterns = ["sample", "test", "pattern"]
+        result = matches_any_pattern(value, patterns)
+        self.assertFalse(result)
 
     def test_matches_any_pattern_empty_patterns(self):
-        """Test with an empty patterns list."""
-        value = "Hello world"
+        value = "Any Text"
         patterns = []
-        self.assertFalse(md.matches_any_pattern(value, patterns))
+        result = matches_any_pattern(value, patterns)
+        self.assertFalse(result)
 
     def test_matches_any_pattern_empty_value(self):
-        """Test with an empty value."""
         value = ""
-        patterns = ["^Hello", "not present", "absent"]
-        self.assertFalse(md.matches_any_pattern(value, patterns))
-
-    @patch('os.path.isdir', return_value=True)
-    @patch('os.listdir', return_value=['file1.txt', 'file2.jpg', 'file3.png'])
-    def test_get_files_from_directory_all_types(self, mock_listdir, mock_isdir):
-        args = Args(directory='/mock_directory', files=None, type=['all'])
-        result = md.get_files(args)
-        self.assertEqual(result, [
-            '/mock_directory/file1.txt',
-            '/mock_directory/file2.jpg',
-            '/mock_directory/file3.png'
-        ])
-
-    @patch('os.path.isdir', return_value=True)
-    @patch('os.listdir', return_value=['file1.txt', 'file2.jpg', 'file3.png'])
-    def test_get_files_from_directory_specific_type(self, mock_listdir, mock_isdir):
-        args = Args(directory='/mock_directory', files=None, type=['jpg'])
-        result = md.get_files(args)
-        self.assertEqual(result, ['/mock_directory/file2.jpg'])
-
-    @patch('os.path.isdir', return_value=False)
-    def test_get_files_nonexistent_directory(self, mock_isdir):
-        args = Args(directory='/nonexistent_directory', files=None, type=['all'])
-        with self.assertRaises(SystemExit) as cm:
-            md.get_files(args)
-        self.assertIn("Error:", str(cm.exception))
-
-    def test_get_files_no_directory(self):
-        args = Args(directory=None, files=['/path/to/file1.txt', '/path/to/file2.jpg'], type=['all'])
-        result = md.get_files(args)
-        self.assertEqual(result, ['/path/to/file1.txt', '/path/to/file2.jpg'])
-
-    @patch('os.path.isdir', return_value=True)
-    @patch('os.listdir', return_value=[])
-    def test_get_files_empty_directory(self, mock_listdir, mock_isdir):
-        args = Args(directory='/empty_directory', files=None, type=['all'])
-        with self.assertRaises(SystemExit) as cm:
-            md.get_files(args)
-        self.assertIn("Error: No files found.", str(cm.exception))
-        
-    def test_display_metadata_all(self):
-        args = Mock(display='all', format='concise')
-        metadata = [
-            {"Field1": "Value1", "Field2": "Value2", "Field3": "Value3"},
-            {"Field1": "Value4", "Field2": "Value5", "Field3": "Value6"}
-        ]
-        ignore_patterns = []
-        
-        expected_output = (
-            "Field1: Value1\nField2: Value2\nField3: Value3\n"
-            "----------------------------------------\n"
-            "Field1: Value4\nField2: Value5\nField3: Value6\n"
-            "----------------------------------------\n"
-        )
-
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            md.display_metadata(args, metadata, ignore_patterns)
-            self.assertEqual(mock_stdout.getvalue(), expected_output)
-
-    @patch('webbrowser.get')
-    def test_open_browser_preferred_browser(self, mock_get):
-        url = "https://www.example.com"
-        preferred_browsers = ["chrome", "firefox"]
-        
-        mock_browser = mock_get.return_value
-        mock_browser.open.return_value = True
-        
-        result = md.open_browser(url, preferred_browsers)
-        
-        self.assertTrue(result)
-        mock_browser.open.assert_called_once_with(url)
-    
-    @patch('webbrowser.get')
-    @patch('webbrowser.open')
-    def test_open_browser_fallback(self, mock_open, mock_get):
-        url = "https://www.example.com"
-        preferred_browsers = ["chrome", "firefox"]
-        
-        mock_get.side_effect = [webbrowser.Error] * len(preferred_browsers)
-        mock_open.return_value = True
-        
-        result = md.open_browser(url, preferred_browsers)
-        
-        self.assertTrue(result)
-        mock_open.assert_called_once_with(url)
-
-    @patch('webbrowser.get')
-    @patch('webbrowser.open')
-    def test_open_browser_failure(self, mock_open, mock_get):
-        url = "https://www.example.com"
-        preferred_browsers = ["chrome", "firefox"]
-        
-        mock_get.side_effect = [webbrowser.Error] * (len(preferred_browsers) + 1)
-        mock_open.side_effect = webbrowser.Error
-        
-        result = md.open_browser(url, preferred_browsers)
-        
+        patterns = ["sample", "test", "pattern"]
+        result = matches_any_pattern(value, patterns)
         self.assertFalse(result)
+
+    def test_get_files_directory_all_types(self):
+        args = Namespace(directory="test_directory", type=['all'], files=[])
         
-if __name__ == "__main__":
+        with unittest.mock.patch('os.path.isdir', return_value=True), \
+             unittest.mock.patch('os.listdir', return_value=['file1.txt', 'file2.jpg', 'file3.png']):
+            result = get_files(args)
+        
+        expected_result = ['test_directory/file1.txt', 'test_directory/file2.jpg', 'test_directory/file3.png']
+        self.assertEqual(result, expected_result)
+
+    def test_get_files_directory_specific_type(self):
+        args = Namespace(directory="test_directory", type=['jpg'], files=[])
+        
+        with unittest.mock.patch('os.path.isdir', return_value=True), \
+             unittest.mock.patch('os.listdir', return_value=['file1.txt', 'file2.jpg', 'file3.png']):
+            result = get_files(args)
+        
+        expected_result = ['test_directory/file2.jpg']
+        self.assertEqual(result, expected_result)
+
+    def test_get_files_nonexistent_directory(self):
+        args = Namespace(directory="nonexistent_directory", type=['all'], files=[])
+        
+        with unittest.mock.patch('os.path.isdir', return_value=False):
+            with self.assertRaises(SystemExit) as cm:
+                get_files(args)
+            self.assertEqual(cm.exception.code, f"Error: {args.directory} is not a directory.")
+
+    def test_get_files_no_files_found(self):
+        args = Namespace(directory="empty_directory", type=['all'], files=[])
+        
+        with unittest.mock.patch('os.path.isdir', return_value=True), \
+             unittest.mock.patch('os.listdir', return_value=[]):
+            with self.assertRaises(SystemExit) as cm:
+                get_files(args)
+            self.assertEqual(cm.exception.code, "Error: No files found.")
+
+    def test_get_files_files_argument(self):
+        args = Namespace(directory=None, type=['all'], files=['file1.txt', 'file2.jpg'])
+        
+        result = get_files(args)
+        expected_result = ['file1.txt', 'file2.jpg']
+        self.assertEqual(result, expected_result)
+
+    def test_get_address_from_coords_valid(self):
+        lat = "40.7128"
+        lon = "-74.0060"
+        
+        with unittest.mock.patch('http.client.HTTPSConnection') as mocked_conn:
+            mocked_response = unittest.mock.Mock()
+            mocked_response.read.return_value = json.dumps({"display_name": "New York, USA"}).encode("utf-8")
+            mocked_conn.return_value.getresponse.return_value = mocked_response
+            result = get_address_from_coords(lat, lon)
+        
+        expected_result = "New York, USA"
+        self.assertEqual(result, expected_result)
+
+    def test_get_address_from_coords_error(self):
+        lat = "40.7128"
+        lon = "-74.0060"
+        
+        with unittest.mock.patch('http.client.HTTPSConnection', side_effect=Exception):
+            result = get_address_from_coords(lat, lon)
+        
+        expected_result = ""
+        self.assertEqual(result, expected_result)
+
+if __name__ == '__main__':
     unittest.main()
